0b1c5e3a722f4b3e67e5df6435b6168f53b319f1
ecountry93 <145100931+ecountry93@users.noreply.github.com>
2024-03-12 17:53:02 +0200

    Update and rename AplicatieGestiuneDepozitApplication.java to AplicatieGestiuneDepozit.java

--- src/main/java/com/example/api/AplicatieGestiuneDepozit.java
+++ src/main/java/com/example/api/AplicatieGestiuneDepozit.java
@@ -8,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
-public class AplicatieGestiuneDepozitApplication {
+public class AplicatieGestiuneDepozit {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AplicatieGestiuneDepozitApplication.class, args);
